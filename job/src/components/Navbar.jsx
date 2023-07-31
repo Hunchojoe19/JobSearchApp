882d215cc6c08c5bc5bcd031a22912fc07837242
Joseph Enubiak <enubiakjoseph@gmail.com>
2023-07-31 11:01:19 +0100

    ...

--- job/src/components/Navbar.jsx
+++ job/src/components/Navbar.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import { useSelector } from "react-redux";
+import { Avatar, Badge } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
 
 const Navbar = () => {
   const [open, setOpen] = React.useState("");
+
+  const { userDetails: select } = useSelector((state) => state);
+  console.log("selkect ", select);
+  const username = select.details.username;
+  const email = select.details.email;
 
   return (
     <nav className="relative container mx-auto p-6 ">
@@ -10,26 +47,63 @@ const Navbar = () => {
         <div className="pt-2 w-64 h-4 flex justify-center items-center">
           <img src={logo} alt="logo" />
         </div>
-        <div className="hidden md:flex space-x-10 text-1.5xl font-['Inter']">
-          <a href="/home" className="hover:text-darkGrayishBlue">
-            Home
-          </a>
-          <a href="/jobs" className="hover:text-darkGrayishBlue">
-            Search Jobs
-          </a>
-          <a href="/login" className="hover:text-darkGrayishBlue">
-            Recruiting?
-          </a>
-          {/* <a href="" className="hover:text-darkGrayishBlue">
-            Follow Us
-          </a> */}
-        </div>
-        <a
-          href="/auth"
-          className="hidden md:block p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight lg:px-12 lg:text-lg"
-        >
-          Job ?
-        </a>
+        {!username && (
+          <div className="hidden md:flex md:space-x-10 md:justify-center md:items-center">
+            <div className="hidden md:flex space-x-10 text-1.5xl font-['Inter'] md:mt-4 md:mr-12">
+              <a href="/home" className="hover:text-darkGrayishBlue">
+                Home
+              </a>
+              <a href="/jobs" className="hover:text-darkGrayishBlue">
+                Search Jobs
+              </a>
+              <a href="/login" className="hover:text-darkGrayishBlue">
+                Recruiting?
+              </a>
+            </div>
+            <a
+              href="/auth"
+              className="hidden md:block p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight lg:px-12 lg:text-lg"
+            >
+              Job ?
+            </a>
+          </div>
+        )}
+
+        {username && (
+          <div className="hidden md:flex justify-center items-center">
+            <div className="hidden md:flex space-x-10 text-1.5xl mr-12 font-['Inter']">
+              <a href="/home" className="hover:text-darkGrayishBlue">
+                Home
+              </a>
+              <a href="/jobs" className="hover:text-darkGrayishBlue">
+                Search Jobs
+              </a>
+            </div>
+            <StyledBadge
+              overlap="circular"
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              variant="dot"
+              marginLeft="2rem"
+            >
+              <Avatar src="/broken-image.jpg" sx={{ marginLeft: "1rem" }} />
+            </StyledBadge>
+
+            <div className="ml-4">
+              <p className="text-base  text-darkGrayishBlue">
+                user id: {(" ", username)}
+              </p>
+              <p className="text-base text-darkGrayishBlue">{email}</p>
+            </div>
+            <div className="ml-24">
+              <a
+                href="/auth"
+                className="px-12 py-4 bg-blue-600 rounded-full text-white font-bold font-['Inter'] hover:bg-blue-400"
+              >
+                Logout
+              </a>
+            </div>
+          </div>
+        )}
         <button
           id="menu-btn"
           className={`block hamburger md:hidden focus:outline-none ${
